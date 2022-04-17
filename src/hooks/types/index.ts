93e9662e6f8bb96631cea072c10ee3d17666fd05
dingym <172635616@qq.com>
2022-04-17 22:09:27 +0800

    fix: 修改 interface 中的索引签名为 extends Record<string | number | symbol, any>

--- src/hooks/types/index.ts
+++ src/hooks/types/index.ts
@@ -1,6 +1,6 @@
-export interface TreeNodeCommon {
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+export interface TreeNodeCommon extends Record<string | number | symbol, any> {
   children?: Array<TreeNodeCommon>;
-  [field: string]: unknown;
 }
 
 export interface ProcessTreeOptions {
