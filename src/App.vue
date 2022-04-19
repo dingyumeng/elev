09bfbad6c93925038544e0e84afe978e89e68703
dingym <172635616@qq.com>
2022-04-19 18:06:06 +0800

    chore: 设置 #app 的高度为 100%

--- src/App.vue
+++ src/App.vue
@@ -10,3 +10,9 @@ import { Vue, Component } from 'vue-property-decorator';
 @Component({ name: 'App' })
 export default class App extends Vue {}
 </script>
+
+<style lang="scss">
+#app {
+  height: 100%;
+}
+</style>
