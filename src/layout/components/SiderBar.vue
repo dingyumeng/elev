8e67748d6e4196f46929169c903b5be012754514
dingym <172635616@qq.com>
2022-04-19 18:07:40 +0800

    feat: 新增 SideBar 组件

---
+++ src/layout/components/SiderBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <aside class="layout-sider">
+    <div class="layout-sider-menu">
+      <ElMenu default-active="2">
+        <ElSubmenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span slot="title">导航一</span>
+          </template>
+          <ElMenuItem index="1-1">菜单一</ElMenuItem>
+        </ElSubmenu>
+        <ElMenuItem index="2">菜单二</ElMenuItem>
+      </ElMenu>
+    </div>
+  </aside>
+</template>
+
+<style lang="scss" scoped>
+.layout-sider {
+  width: 208px;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  box-shadow: 2px 0 8px 0 rgba(29, 35 , 41, .05);
+}
+</style>
---
+++ src/layout/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="layout-wrapper">
+    <SiderBar />
+    <div class="layout-container">
+      <!-- <router-view></router-view> -->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import SiderBar from './components/SiderBar.vue';
+
+@Component({
+  name: 'Layout',
+  components: {
+    SiderBar,
+  }
+})
+export default class Layout extends Vue {}
+</script>
+
+<style lang="scss">
+.layout-wrapper {
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+}
+</style>
