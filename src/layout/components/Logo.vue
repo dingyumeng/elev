cede49ae9fe3b904bb59b3c85ffa95cd0d35dd57
dingym <172635616@qq.com>
2022-04-19 23:45:44 +0800

    feat: 新增 Logo 组件

---
+++ src/layout/components/Logo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :class="layoutWrapperClass">
+    <a href="javascript:void(0);">
+      <img src="/logo.svg" alt="logo" />
+      <h1>EleV Pro</h1>
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+// const classNames = ['layout-header-logo', 'layout-sider-logo', 'small', 'normal'];
+
+@Component({ name: 'Header' })
+export default class Header extends Vue {
+  layoutWrapperClass = {
+    'layout-header-logo': true,
+    small: true,
+  };
+}
+</script>
+
+<style lang="scss">
+.layout-header-logo {
+  display: flex;
+  min-width: 192px;
+
+  a {
+    display: flex;
+    align-items: center;
+
+    img {
+      height: 28px;
+    }
+
+    h1 {
+      color: #ffffff;
+      font-size: 18px;
+      font-weight: 600;
+      height: 32px;
+      line-height: 32px;
+      margin: 0 0 0 12px;
+    }
+  }
+}
+</style>
